9cdae8cb43b7d1da70bb04c992a7b93b77f2d1a2
NaNyeS2 <hyunkyungj0730@gmail.com>
2023-03-28 14:30:20 +0900

    4주차

---
+++ JAVA practice/2023 CH03/src/Week4_20201458.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Week4_20201458 {
+    public static double celsiusToFahrenheit(double celsius){
+        return celsius * 1.8 + 32;
+    }
+    public static double fahrenheitToCelsius(double fahrenheit){
+        return (fahrenheit - 32) / 1.8;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String source, target;
+        double cel, fah;
+        System.out.println("Hello!");
+        do{
+            System.out.print("Please choose a source unit (C,F) or type 'exit' to quit : ");
+            source = in.nextLine();
+            if (source.equalsIgnoreCase("exit"))
+                break;
+            else if (source.equalsIgnoreCase("F")){
+                System.out.print("Enter the temperature in Fahrenheit: ");
+                fah = in.nextDouble();
+                cel = fahrenheitToCelsius(fah);
+                System.out.println(fah + " Fahrenheit is " + cel + " Celsius.");
+            }
+            else if (source.equalsIgnoreCase("C")){
+                System.out.print("Enter the temperature in Celsius: ");
+                cel = in.nextDouble();
+                fah = celsiusToFahrenheit(cel);
+                System.out.println(cel + " Celsius is " + fah + " Fahrenheit.");
+            }
+        }while(true);
+        System.out.println("Good bye!");
+    }
+}
